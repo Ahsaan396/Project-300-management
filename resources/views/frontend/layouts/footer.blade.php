24371929da358675fdb06cf3d9cc5739cd6f54e2
Ahbab <ahbabshahin7788@gmail.com>
2021-09-26 14:27:02 +0600

    Dashboard Slicing

---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+      <strong>Copyright &copy; 2021-2040 <a href="https://metrouni.edu.bd/">Metropolitan University</a>.</strong>
+      All rights reserved.
+      <div class="float-right d-none d-sm-inline-block">
+        <b>Designed & Developed by</b> Ahbab & Ahsanul
+      </div>
+</footer>
---
+++ resources/views/frontend/layouts/navbar.blade.php
@@ -0,0 +1,74 @@
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+      <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    </div>
+  
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+  
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="{{route('login')}}" class="nav-link">Login</a>
+        </li>
+  
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="{{route('register')}}" class="nav-link">Register</a>
+        </li>
+  
+      </ul>
+  
+      <!-- Right navbar links -->
+      <ul class="navbar-nav ml-auto">
+        <!-- Navbar Search -->
+        <li class="nav-item">
+          <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+            <i class="fas fa-search"></i>
+          </a>
+          <div class="navbar-search-block">
+            <form class="form-inline">
+              <div class="input-group input-group-sm">
+                <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                  <button class="btn btn-navbar" type="submit">
+                    <i class="fas fa-search"></i>
+                  </button>
+                  <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </li>
+  
+        <!-- Notifications Dropdown Menu -->
+        <li class="nav-item ">
+          <div>
+                    <form action="{{ route('logout') }}" method="POST" id="logout-form">
+                          @csrf
+                          <a href="route('logout')"
+                          onclick="event.preventDefault();
+                                      this.closest('form').submit();" class="dropdown-item dropdown-footer">Logout</a>
+                     </form>
+          </div>
+        </li>
+  
+        <li class="nav-item">
+          <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+            <i class="fas fa-expand-arrows-alt"></i>
+          </a>
+        </li>
+  
+        <li class="nav-item">
+          <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+            <i class="fas fa-th-large"></i>
+          </a>
+        </li>
+  
+      </ul>
+    </nav>
+      <!-- /.navbar -->
